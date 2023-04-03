121255c42090404c1d4abae9a76a6be1ea2e2acd
soohyoen <gnejfejf789@naver.com>
2023-04-03 15:17:26 +0900

    RT imutils + cv2 function

--- BEV/pyimagesearch/Real-time stitching/RT_method.py
+++ BEV/pyimagesearch/Real-time stitching/RT_method.py
@@ -1,3 +1,4 @@
+"""
 from imutils.video import VideoStream
 import cv2
 import imutils
@@ -5,11 +6,32 @@ import imutils
 
 cv2.namedWindow("111")
 cap = cv2.VideoCapture(0)
+
 while(cap.isOpened()):
     ret, img = cap.read()
     # real time일 때, img를 기반으로 추가정보 설정이 가능함.
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     cv2.imshow("111", img)
     k = cv2.waitKey(10)
     if k == 27:
        break
+
+
+"""
+
+from imutils.video import VideoStream
+import numpy as np
+import datetime
+import imutils
+import time
+import cv2
+import argparse
+
+cv2.namedWindow("111")
+stream = VideoStream(src=0).start()
+time.sleep(2.0)
+
+while True:
+    stream123 = stream.read()
+    cv2.imshow("111", stream123)
+    k = cv2.waitKey(10)
+
